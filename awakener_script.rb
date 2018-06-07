930a42afedaaca25a5d35b4f0820ee906da66a04
Paul M2B <polo2bodi@gmail.com>
2018-06-07 17:42:07 +0200

    create class ReviewApp

--- awakener_script.rb
+++ awakener_script.rb
@@ -2,29 +2,66 @@ require 'rubygems'
 require 'bundler/setup'
 require 'platform-api'
 
-def review_apps_to_wake_up
-  PlatformAPI.connect(ENV['HEROKU_API_KEY']).app.list.map do |app|
-    app["name"] if app["name"].include?("sirac-staging-pr-")
-  end.compact
-end
+class ReviewApp
+  REVIEWAPP_REGEX = /-pr-\d+\z/
 
-def with_maintenance_mode(app)
-  system "heroku maintenance:on --app #{app}"
-  yield
-  system "heroku maintenance:off --app #{app}"
-end
+  attr_reader :name
 
-def capture_database(app)
-  system "heroku pg:backups:capture --app #{app}"
-end
+  class << self
+    def for(*parent_app_names)
+      all.select do |app|
+        parent_app_names.any? do |parent_app_name|
+          app.name.match? Regexp.new(parent_app_name + REVIEWAPP_REGEX.to_s)
+        end
+      end
+    end
 
-def restore_database(app)
-  system "heroku pg:backups:restore --app {app} --confirm #{app}"
-end
+  private
 
-review_apps_to_wake_up.each do |app|
-  with_maintenance_mode(app) do
-    capture_database(app)
-    restore_database(app)
+    def all
+      find_all_names.map do |app_name|
+        new(app_name)
+      end
+    end
+
+    def find_all_names
+      PlatformAPI.connect(ENV["HEROKU_API_KEY"]).app.list.map do |app|
+        app["name"] if app["name"].match? REVIEWAPP_REGEX
+      end.compact
+    end
   end
+
+  def initialize(name)
+    @name = name
+  end
+
+  def wake_up
+    with_maintenance_mode do
+      capture_database
+      restore_database
+    end
+  end
+
+private
+
+  def with_maintenance_mode
+    system "heroku maintenance:on --app #{name}"
+    yield
+    system "heroku maintenance:off --app #{name}"
+  end
+
+  def capture_database
+    system "heroku pg:backups:capture --app #{name}"
+  end
+
+  def restore_database
+    system "heroku pg:backups:restore --app #{name} --confirm #{name}"
+  end
+end
+
+if ENV["PARENT_APPS"]
+  ReviewApp.for(*ENV["PARENT_APPS"].split).map(&:wake_up)
+else
+  puts "Error: PARENT_APPS must be set"
+  exit 78 # EX_CONFIG
 end
